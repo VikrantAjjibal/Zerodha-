e14067afcc45fef4ea1fb9373f26fea608546e84
Vikrant Hegde <vikrantajjibal@gmail.com>
2019-06-02 13:54:23 +0530

    Added Requirement.txt

--- disp_table.py
+++ disp_table.py
@@ -44,5 +44,5 @@ class HelloWorld(object):
     index.exposed = True
 
 # bind to all IPv4 interfaces
-cherrypy.config.update({'server.socket_host': '10.199.8.150'})
+cherrypy.config.update({'server.socket_host': 'https://zerodhatest.herokuapp.com/'})
 cherrypy.quickstart(HelloWorld())
